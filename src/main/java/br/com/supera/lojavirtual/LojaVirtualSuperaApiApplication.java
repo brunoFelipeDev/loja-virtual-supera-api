75ad5c4c3f3fb024bad4697d15c22edd12fed730
Bruno Felipe <brulip.m@gmail.com>
2021-06-14 16:58:38 -0300

    Init

--- src/main/java/br/com/supera/lojavirtual/LojaVirtualSuperaApiApplication.java
+++ src/main/java/br/com/supera/lojavirtual/LojaVirtualSuperaApiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LojaVirtualSuperaApiApplication {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaVirtualSuperaApiApplication.class, args);
