433cd9886841c3e68a8b27a6fcebefb8de762229
Bruno Felipe <brulip.m@gmail.com>
2021-06-14 16:49:52 -0300

    Primeiro commit

---
+++ src/main/java/br/com/supera/lojavirtual/LojaVirtualSuperaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.supera.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualSuperaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualSuperaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/supera/lojavirtual/LojaVirtualSuperaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.supera.lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualSuperaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
